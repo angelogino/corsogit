687f875da9ec03279bafaa29810c6624e9010aaa
gino <gino@gmail.com>
2021-04-01 11:57:35 +0200

    riaggiunto la funzione molti

--- function.php
+++ function.php
@@ -9,6 +9,14 @@ function addizione (m1,m2){
 function sottrazione (m1,m2){
   return m1-m2;
 }
+/* moltiplicazione */
+function  moltiplicazione (m1,m2){
+  if (m2!== 0;){
+    return m1/m2;
+  }
+  else{
+    return "error";
+  }
 
 /* divisione */
 function  divisione (m1,m2){
